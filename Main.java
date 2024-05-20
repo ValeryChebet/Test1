1a20fe2518b38ceaf563709c0a73b3dea00a46ba
ValeryChebet <valerychebet.ke@gmail.com>
2024-05-20 11:25:39 +0300

    First Commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println("HellO World, Valery");
+        System.out.println("I am learning Java ");
+        System.out.print("I'm gonna be good at it!");
+    }
+}
